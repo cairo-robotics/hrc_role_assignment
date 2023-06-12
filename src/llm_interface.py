12542be19def09e5c48ada69addd0884ff1de688
kalebishop <kaleb.bishop@colorado.edu>
2023-06-12 14:33:29 -0600

    sketch code of prompt generation for role description API calls

--- src/llm_interface.py
+++ src/llm_interface.py
@@ -3,11 +3,11 @@ import openai
 
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
-def get_llm_response(prompt, engine='davinci'):
+def get_llm_response(prompt, engine='davinci', temperature=0.9):
     response = openai.Completion.create(
         engine=engine,
         prompt=prompt,
-        temperature=0.9,
+        temperature=temperature,
         max_tokens=100,
         top_p=1,
         frequency_penalty=0.0,
@@ -15,3 +15,34 @@ def get_llm_response(prompt, engine='davinci'):
         stop=["\n"]
     )
     return response
+
+def generate_prompt_generic(task_list):
+    # few-shot learning description prompt
+    # just for testing -- TODO refine, get better examples
+        # QUESTION: how to maintain context across multiple prompts, if possible?
+    task_dict = {
+        'get_tomato'    : 'get tomatoes from the fridge',
+        'get_onion'     : 'get onions from the fridge',
+        'place_tomato'  : 'place tomatoes in cooking pot',
+        'place_onion'   : 'place onions in cooking pot',
+        'get_dish'      : 'get dish from the cupboard',
+        'place_dish'    : 'place dish on the table',
+        'plate_dish'    : 'plate dish',
+        'serve_dish'    : 'serve dish to customer'
+    }
+
+    tasks_nl = "".join([task_dict[task] + ", " for task in task_list])
+
+    prompt = '''Let's say we're playing the game Overcooked together.\
+                Suggest a role description for the task list.
+                 
+                Task list: get tomatoes from the fridge, get onions from the fridge, place tomatoes in cooking pot, place onions in cooking pot,
+                Role: prep chef
+
+                Task list: get tomatoes from the fridge, place tomatoes in cooking pot,
+                Role: tomato chef
+
+                Task list: {}
+                Role:'''.format(tasks_nl)
+
+    return prompt
